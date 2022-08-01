8a19f43fc4564dc7e7cb61d36bba19e05b73629e
Alejo Boga <alejoboga20@gmail.com>
2022-08-01 13:53:00 -0300

    Create a pokemon in DB

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  constructor(
+    @InjectModel(Pokemon.name) private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
 
-    return { msg: 'This action adds a new pokemon', createPokemonDto };
+    const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+    return { msg: 'This action adds a new pokemon', pokemon };
   }
 
   findAll() {
