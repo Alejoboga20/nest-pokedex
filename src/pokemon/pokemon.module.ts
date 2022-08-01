38e9752bcab815aabd2f75f2124f417815aad8a7
Alejo Boga <alejoboga20@gmail.com>
2022-08-01 11:27:39 -0300

    Create schema and model

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { PokemonService } from './pokemon.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PokemonController } from './pokemon.controller';
+import { PokemonService } from './pokemon.service';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
