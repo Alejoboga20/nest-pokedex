30cfb3ed700870c68ea037d97744a3a933d91589
Alejo Boga <alejoboga20@gmail.com>
2022-08-02 10:29:29 -0300

    Create Http adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeAPIResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios; //axios is a dependency for my service
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany();
 
-    const { data } = await axios.get<PokeAPIResponse>(
+    const data = await this.http.get<PokeAPIResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
