4ea464e295873ac4af7d45be6e265a05ffd46792
Alejo Boga <alejoboga20@gmail.com>
2022-08-03 12:04:53 -0300

    Add nest config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
@@ -9,10 +10,10 @@ import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '/public'),
     }),
-
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
     CommonModule,
