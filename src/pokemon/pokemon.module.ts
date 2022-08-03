3e1c09fdeb04f2e3ad50cb436e921447f3e9331f
Alejo Boga <alejoboga20@gmail.com>
2022-08-03 14:58:24 -0300

    Use configservice

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { PokemonController } from './pokemon.controller';
 import { PokemonService } from './pokemon.service';
@@ -9,6 +10,7 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
   controllers: [PokemonController],
   providers: [PokemonService],
   imports: [
+    ConfigModule,
     MongooseModule.forFeature([
       {
         name: Pokemon.name,
